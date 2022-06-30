16a97069573d2e70343e236db4b0ec67c842be88
Henok Mekonnen <henatan99@yahoo.com>
2022-06-30 03:24:52 -0400

    Add initial code in index.js and Procfile

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to factorial calculator'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.status(404).json({
+        message: 'Resource not found'
+    });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(
+    `Listening on port ${port}`
+));
