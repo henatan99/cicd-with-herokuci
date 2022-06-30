c2d45c53a67cce851e79f7a3b118c00b1d1fabba
Henok Mekonnen <henatan99@yahoo.com>
2022-06-30 04:02:21 -0400

    Factorial service

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const factorial = require('./factorial');
 
 const app = express();
 
 app.get('/', (req, res) => {
+    const { host } = req.headers;
     res.status(200).json({
         message: 'Welcome to factorial calculator'
     });
 });
+
+app.get('/docs', (req, res) => {
+    const { host } = req.headers;
+    res.status(200).json({
+        message: 'Documentaion',
+        request: `http://${host}/factorial/3`,
+        response: 'The factorial of 3 is 6'
+    });
+});
+
+app.get('/factorial/:number', (req, res) => {
+    const { number } = req.params;
+    if(isNaN(number)) return res.status(400).json({
+        message: `'${req.params.number}' is not a number.`
+    });
+    if (number > 200) return res.status(200).json({
+        message: `The factorial of ${number} is Infinity` });
+    return res.status(200).json({ 
+        message: `The factorial of ${number} is ${factorial(number)}` });  
+})
 
 app.get('*', (req, res) => {
     res.status(404).json({
